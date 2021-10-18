5fa00637a65d52cea7c1eb1ba0b0fc2fea028eac
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-10-18 11:35:50 +0530

    add code for method

--- src/LinkedList/SinglyLinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList/SinglyLinkedList.java
@@ -252,7 +252,13 @@ public class SinglyLinkedList {
 
     public void removeDuplicatesInSortedList() {
         ListNode current = head;
-
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
     }
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
