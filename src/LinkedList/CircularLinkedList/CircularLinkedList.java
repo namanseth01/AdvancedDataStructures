107ef1c0923dfad7910badd420ac500d1f573311
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-11-10 11:27:20 +0530

    edit

--- src/LinkedList/CircularLinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularLinkedList/CircularLinkedList.java
@@ -7,5 +7,35 @@
 
 package LinkedList.CircularLinkedList;
 
+import java.util.List;
+
 public class CircularLinkedList {
+    private ListNode last;
+    private int length;
+
+
+    private class ListNode {
+        private ListNode next;
+        private int data;
+
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void createCircularLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+    }
 }
