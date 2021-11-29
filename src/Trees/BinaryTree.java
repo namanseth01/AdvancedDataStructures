cdc531ab12b3f151fc949da62b8fdc13e67eb019
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-29 11:14:38 +0530

    add definitiion

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -1,4 +1,16 @@
 package Trees;
 
 public class BinaryTree {
+    private TreeNode root;
+
+    private class TreeNode {
+        private Trees.TreeNode left;
+        private Trees.TreeNode right;
+        private int data;
+
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
 }
