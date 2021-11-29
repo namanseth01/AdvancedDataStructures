bb8caee1c2793c13189411d79c5b7d63b7e9470d
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-29 11:12:03 +0530

    add treeNOde class definition

--- src/Trees/TreeNode.java
+++ src/Trees/TreeNode.java
@@ -1,5 +1,12 @@
 package Trees;
 
 public class TreeNode {
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
 
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
 }
